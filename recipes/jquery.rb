c889de733abd2db2c7df2f221068726e0572d990
Sam Umbach <sam@thinkrelevance.com>
2011-02-21 00:41:10 -0500

    Add jQuery and jQuery UI to default Javascript files; switch to jQuery-compatible rails.js implementation.

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -10,5 +10,11 @@ get "http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js",      "pub
 get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.js",     "public/javascripts/jquery-ui.js"
 get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
 
+# replace rails.js
+remove_file "public/javascripts/rails.js"
+get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+
+gsub_file "config/application.rb", /# (config\.action_view\.javascript_expansions\[:defaults\]).*$/, '\1 = %w(jquery.min jquery-ui.min rails)'
+
 git :add => "."
 git :commit => "-a -m 'Replace Prototype and script.aculo.us with jQuery and jQuery UI'"
