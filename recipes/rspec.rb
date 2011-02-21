f6089a15abf72d9c15f178faa6f3a4041f284264
Sam Umbach <sam@thinkrelevance.com>
2011-02-20 21:25:41 -0500

    Move initialization code to separate files

--- recipes/rspec.rb
+++ recipes/rspec.rb
@@ -13,23 +13,23 @@ rvm_run "./script/rails generate rspec:install"
 gsub_file "spec/spec_helper.rb", /# (config.mock_with :mocha)/,   '\1'
 gsub_file "spec/spec_helper.rb",   /(config.mock_with :rspec)/, '# \1'
 
-inject_into_file "spec/spec_helper.rb", :after => /RSpec\.configure.*\n/ do
-  <<-CODE
+initializer "rspec.rb", <<-CODE
+Rails.application.config.generators.test_framework :rspec, :fixture => false
+CODE
+file "spec/support/focused.rb", <<-CODE
+RSpec.configure do |config|
   # Configure RSpec to run focused specs, and also respect the alias 'fit' for focused specs
   config.filter_run :focused => true
   config.run_all_when_everything_filtered = true
   config.alias_example_to :fit, :focused => true
-
+end
+CODE
+file "spec/support/color.rb", <<-CODE
+RSpec.configure do |config|
   # Turn color on if we are NOT inside Textmate, Emacs, or VIM
   config.color_enabled = ENV.keys.none? { |k| %w{TM_MODE EMACS VIM}.include? k }
-
-  CODE
 end
-inject_into_file "config/application.rb", :after => /config\.generators.*\n/ do
-  <<-CODE
-      g.test_framework  :rspec, :fixture => false
-  CODE
-end
+CODE
 
 git :add => "."
 git :commit => "-a -m 'Replace Test::Unit with Rspec'"
