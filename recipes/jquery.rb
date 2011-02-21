ea81ebaa4bf1c131619bf5947bf65591013cf978
Sam Umbach <sam@thinkrelevance.com>
2011-02-20 20:39:59 -0500

    Add uncompressed versions of jQuery and jQuery UI

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -5,7 +5,9 @@ remove_file "public/javascripts/effects.js"
 remove_file "public/javascripts/prototype.js"
 
 # install jQuery and jQuery UI
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.js",          "public/javascripts/jquery.js"
 get "http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js",      "public/javascripts/jquery.min.js"
+get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.js",     "public/javascripts/jquery-ui.js"
 get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
 
 git :add => "."
