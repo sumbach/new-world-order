ed383a3fa27ce2d8c5fa72a3a4f3b35091036e44
Sam Umbach <sam@thinkrelevance.com>
2011-02-20 20:39:28 -0500

    Upgrade to latest jQuery and jQuery UI

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -5,8 +5,8 @@ remove_file "public/javascripts/effects.js"
 remove_file "public/javascripts/prototype.js"
 
 # install jQuery and jQuery UI
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",      "public/javascripts/jquery.min.js"
-get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js",      "public/javascripts/jquery.min.js"
+get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js", "public/javascripts/jquery-ui.min.js"
 
 git :add => "."
 git :commit => "-a -m 'Replace Prototype and script.aculo.us with jQuery and jQuery UI'"
